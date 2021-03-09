62f64e63a4a73b3eaabff9e4a6163fad13596bc7
taigaoma1997 <78004072+taigaoma1997@users.noreply.github.com>
2021-03-08 23:39:09 -0500

    update

--- RCWA/MODEL_structure_simulator.m
+++ RCWA/MODEL_structure_simulator.m
@@ -1,14 +1,13 @@
 
-
-model = 'vae';
+% so far we only need to simulate for models: vae, inn, inverse, tandem. 
+model = 'tandem';  
 filename = strcat('data_predicted\param_', model, '_pred.mat');
-filename_save = strcat('data_predicted\spectrum\param_', model, '_pred_simulate.mat');
+filename_save = strcat('data_predicted\spectrum\spectrum_param_', model, '_pred.mat');
 
 load(filename);
 
 M = size(param_pred,1);
 N = size(param_pred,2);
-N_average = int16( N/4);
 param_pred_re = reshape(param_pred, [], 4);
 
 START = 1;
@@ -20,7 +19,7 @@ stepcase = 5;
 show1 = 0;
 
 spectrum= [];
-for i = START:1:END
+for i = START:1:END   % we start from the last data in case the simulation was stopped at the saving part. 
     refls = RCWA_Silicon(param_pred_re(i,1), param_pred_re(i,2), param_pred_re(i,3), param_pred_re(i,4), acc, show1,stepcase);
     spectrum(i,:) = refls;
     CURRENT = i;
@@ -28,4 +27,6 @@ for i = START:1:END
     i
 end
 
-    
+CURRENT = CURRENT +1;
+
+fprintf('Simulation done! \n');
--- RCWA/MODEL_structure_simulator_recover.m
+++ RCWA/MODEL_structure_simulator_recover.m
@@ -1,20 +1,26 @@
-
-model = 'vae';
+% so far we only need to simulate for models: vae, inn, inverse, tandem. 
+model = 'tandem';
 filename = strcat('data_predicted\param_', model, '_pred.mat');
-filename_save = strcat('data_predicted\spectrum\param_', model, '_pred_simulate.mat');
+filename_save = strcat('data_predicted\spectrum\spectrum_param_', model, '_pred.mat');
 
 load(filename);
-load(filename_save)
+load(filename_save);
 param_pred_re = reshape(param_pred, [], 4);
 
 acc = 10;
 stepcase = 5;
 show1 = 0;
 
-for i = CURRENT:1:END   % we start from the last data in case the simulation was stopped at the saving part. 
+if CURRENT>END
+    fprintf('This simulation is already done! \n');
+else
+    for i = CURRENT:1:END   % we start from the last data in case the simulation was stopped at the saving part. 
     refls = RCWA_Silicon(param_pred_re(i,1), param_pred_re(i,2), param_pred_re(i,3), param_pred_re(i,4), acc, show1,stepcase);
     spectrum(i,:) = refls;
     CURRENT = i;
     save(filename_save,'spectrum', 'START','END', 'CURRENT');
     i
+    end
 end
+
+fprintf('Simulation done! \n');
